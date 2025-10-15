e3ade3e838c068d3f3b0726573cf54ba81b46a21
Carlos Rojas <cirojas6@uc.cl>
2025-10-15 17:27:45 -0300

    2 bfs algorithms working

--- src/system/path_manager.cc
+++ src/system/path_manager.cc
@@ -182,6 +182,14 @@ ObjectId PathManager::set_path(const Paths::ShortestKGroupsWalks::SearchState* v
     return ObjectId(ObjectId::MASK_PATH | SHORTEST_K_GROUPS_WALKS_MASK | path_var.id);
 }
 
+ObjectId PathManager::set_path(const Paths::Any::MultiSourceSearchState* visited_pointer, VarId path_var)
+{
+    auto index = get_thread_index();
+    paths[index][path_var.id] = visited_pointer;
+    return ObjectId(ObjectId::MASK_PATH | BFS_MULTIPLE_STARTS_MASK | path_var.id);
+}
+
+
 void PathManager::for_each(
     uint64_t path_id,
     std::function<void(ObjectId)> node_func,
@@ -304,6 +312,13 @@ void PathManager::for_each(
     }
     case SHORTEST_K_GROUPS_WALKS_MASK: {
         auto state = reinterpret_cast<const Paths::ShortestKGroupsWalks::SearchState*>(
+            paths[index][decoded_id]
+        );
+        state->for_each(node_func, edge_func, begin_at_left[index][decoded_id]);
+        break;
+    }
+    case BFS_MULTIPLE_STARTS_MASK: {
+        auto state = reinterpret_cast<const Paths::Any::MultiSourceSearchState*>(
             paths[index][decoded_id]
         );
         state->for_each(node_func, edge_func, begin_at_left[index][decoded_id]);
